69d6785e905d6e3c2bf1612df40076ce14cad492
Hanzla07ta <168344524+Hanzla07ta@users.noreply.github.com>
2024-05-12 12:05:12 +0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,53 @@
+def encrypt(text, shift):
+    encrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) + shift
+            if char.islower():
+                if shifted > ord('z'):
+                    shifted -= 26
+            elif char.isupper():
+                if shifted > ord('Z'):
+                    shifted -= 26
+            encrypted_text += chr(shifted)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
+def decrypt(text, shift):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) - shift
+            if char.islower():
+                if shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted < ord('A'):
+                    shifted += 26
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+def main():
+    while True:
+        choice = input("Enter 'e' for encryption, 'd' for decryption or 's' for stop execution: ").lower()
+        if choice == 'e':
+            message = input("Enter the message to encrypt: ")
+            shift = int(input("Enter the shift value: "))
+            encrypted_message = encrypt(message, shift)
+            print("Encrypted message:", encrypted_message)
+        elif choice == 'd':
+            message = input("Enter the message to decrypt: ")
+            shift = int(input("Enter the shift value: "))
+            decrypted_message = decrypt(message, shift)
+            print("Decrypted message:", decrypted_message)
+        elif choice == 's':
+            message = input("Process finished. ")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
